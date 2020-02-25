515bc0569540b440f4829b55c565a6fadd8bd09c
Artem Simonenko <artem.simonenko93@gmail.com>
2020-02-25 15:51:26 +0200

    Fixed dissmiss icon red line

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -16,39 +16,59 @@ import { paths } from "../gulpfile.babel";
 const { argv } = yargs;
 const production = !!argv.production;
 
-gulp.task("styles", () => gulp.src(paths.styles.src)
-    .pipe(gulpif(!production, sourcemaps.init()))
-    .pipe(plumber())
-    .pipe(sass({ includePaths: ["node_modules"] }))
-    .pipe(groupmedia())
-    .pipe(gulpif(production, autoprefixer({
-        cascade: false,
-        grid: true,
-    })))
-    .pipe(gulpif(production, mincss({
-        compatibility: "ie8",
-        level: {
-            1: {
-                specialComments: 0,
-                removeEmpty: true,
-                removeWhitespace: true,
-            },
-            2: {
-                mergeMedia: true,
-                removeEmpty: true,
-                removeDuplicateFontRules: true,
-                removeDuplicateMediaBlocks: true,
-                removeDuplicateRules: true,
-                removeUnusedAtRules: false,
-            },
-        },
-    })))
-    .pipe(gulpif(production, rename({
-        suffix: ".min",
-    })))
-    .pipe(gulpif(!production, sourcemaps.write("./maps/")))
-    .pipe(gulp.dest(paths.styles.dist))
-    .pipe(debug({
-        title: "CSS files",
-    }))
-    .pipe(browsersync.stream()));
+gulp.task("styles", () =>
+    gulp
+        .src(paths.styles.src)
+        .pipe(gulpif(!production, sourcemaps.init()))
+        .pipe(plumber())
+        .pipe(sass({ includePaths: ["node_modules"] }))
+        .pipe(groupmedia())
+        .pipe(
+            gulpif(
+                production,
+                autoprefixer({
+                    cascade: false,
+                    grid: true,
+                })
+            )
+        )
+        .pipe(
+            gulpif(
+                production,
+                mincss({
+                    compatibility: "ie8",
+                    level: {
+                        1: {
+                            specialComments: 0,
+                            removeEmpty: true,
+                            removeWhitespace: true,
+                        },
+                        2: {
+                            mergeMedia: true,
+                            removeEmpty: true,
+                            removeDuplicateFontRules: true,
+                            removeDuplicateMediaBlocks: true,
+                            removeDuplicateRules: true,
+                            removeUnusedAtRules: false,
+                        },
+                    },
+                })
+            )
+        )
+        .pipe(
+            gulpif(
+                production,
+                rename({
+                    suffix: ".min",
+                })
+            )
+        )
+        .pipe(gulpif(!production, sourcemaps.write("./maps/")))
+        .pipe(gulp.dest(paths.styles.dist))
+        .pipe(
+            debug({
+                title: "CSS files",
+            })
+        )
+        .pipe(browsersync.stream())
+);
